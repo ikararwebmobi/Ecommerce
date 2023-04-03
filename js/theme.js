cf5fa7e80820f1e2a16bb24049352e397c341f1c
ikararwebmobi <ikarar.webmobi@gmail.com>
2023-04-03 11:00:14 +0530

    homepage created

--- js/theme.js
+++ js/theme.js
@@ -14,6 +14,41 @@ $(".topbar-offer-slider").slick({
   slidesToScroll: 1,
   vertical: true,
   centerMode: true,
+  dots:false,
+  arrows: false,
   centerPadding: "0",
   autoplay: true,
 });
+
+$(".product-slider").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+ // autoplay: true,
+  draggabley: true,
+  swipe: true,
+  dots: true,
+  arrows: true,
+  centerPadding: '40px',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
